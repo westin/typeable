4527df2ad0545b318d42b6bb869378c96dfe0b8d
Westin Lohne <westin@cmu.edu>
2015-11-14 00:58:23 -0500

    starter code

---
+++ Plum-o-Meter/ViewController.swift
@@ -0,0 +1,197 @@
+//
+//  ViewController.swift
+//  Plum-o-Meter
+//
+//  Created by Simon Gladman on 24/10/2015.
+//  Copyright © 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox // Needed Vibrate the iPhone
+
+class ViewController: UIViewController
+{
+    
+    let label = UILabel()
+    
+    var circles = [UITouch: CircleWithLabel]()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        view.multipleTouchEnabled = true
+        
+        label.text = "lay your plums on me."
+        
+        label.textAlignment = NSTextAlignment.Center
+        
+        view.addSubview(label)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        label.hidden = true
+        
+        for touch in touches
+        {
+            let circle = CircleWithLabel()
+            
+            circle.drawAtPoint(touch.locationInView(view),
+                force: touch.force / touch.maximumPossibleForce)
+            
+            
+            circles[touch] = circle
+            view.layer.addSublayer(circle)
+        }
+        
+        highlightHeaviest()
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        for touch in touches where circles[touch] != nil
+        {
+            let circle = circles[touch]!
+            
+            circle.drawAtPoint(touch.locationInView(view),
+                force: touch.force / touch.maximumPossibleForce)
+        }
+        
+        highlightHeaviest()
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        for touch in touches where circles[touch] != nil
+        {
+            let circle = circles[touch]!
+            
+            circles.removeValueForKey(touch)
+            circle.removeFromSuperlayer()
+        }
+        
+        highlightHeaviest()
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?)
+    {
+        guard let touches = touches else
+        {
+            return
+        }
+        
+        for touch in touches where circles[touch] != nil
+        {
+            let circle = circles[touch]!
+            
+            circle.removeFromSuperlayer()
+        }
+    }
+    
+    func highlightHeaviest()
+    {
+        func getMaxTouch() -> UITouch?
+        {
+            return circles.sort({
+                (a: (UITouch, CircleWithLabel), b: (UITouch, CircleWithLabel)) -> Bool in
+                
+                return a.0.force > b.0.force
+            }).first?.0
+        }
+        
+        circles.forEach
+        {
+            $0.1.isMax = $0.0 == getMaxTouch()
+        }
+    }
+    
+//    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask
+//    {
+//        return UIInterfaceOrientationMask.Landscape
+//    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        label.frame = view.bounds
+    }
+}
+
+// -------------
+
+class CircleWithLabel: CAShapeLayer
+{
+    let text = CATextLayer()
+    
+    override init()
+    {
+        super.init()
+        
+        text.foregroundColor = UIColor.blueColor().CGColor
+        text.alignmentMode = kCAAlignmentCenter
+        addSublayer(text)
+        
+        strokeColor = UIColor.blueColor().CGColor
+        lineWidth = 5
+        fillColor = nil
+    }
+    
+    override init(layer: AnyObject)
+    {
+        super.init(layer: layer)
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var isMax: Bool = false
+    {
+        didSet
+        {
+            fillColor = isMax ? UIColor.yellowColor().CGColor : nil
+        }
+    }
+    
+    func drawAtPoint(location: CGPoint, force: CGFloat)
+    {
+        let radius = 120 + (force * 120)
+        
+        path = UIBezierPath(
+            ovalInRect: CGRect(
+                origin: location.offset(dx: radius, dy: radius),
+                size: CGSize(width: radius * 2, height: radius * 2))).CGPath
+        
+        text.string = String(format: "%.1f%%", force * 100)
+        
+        switch force * 100 {
+            case 0:
+                text.string = String(format: "No level")
+            case 0.1..<25:
+                text.string = String(format: "Level 1")
+        case 25..<50:
+            text.string = String(format: "Level 2")
+        case 50..<75:
+            text.string = String(format: "Level 3")
+        case 75..<200:
+            text.string = String(format: "Level 4")
+            print(force * 100)
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+        default:
+            text.string = String(format: "No level")
+        }
+        
+        text.frame = CGRect(origin: location.offset(dx: 75, dy: -radius), size: CGSize(width: 150, height: 40))
+    }
+}
+
+// -------------
+
+extension CGPoint
+{
+    func offset(dx dx: CGFloat, dy: CGFloat) -> CGPoint
+    {
+        return CGPoint(x: self.x - dx, y: self.y - dy)
+    }
+}
